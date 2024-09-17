bdedc15f10b57647c13dc1694fd04f2cd1f9d7c8
duchadev <95076379+hannguu@users.noreply.github.com>
2024-09-17 23:18:48 +0700

    first stage

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        {/* <h5 className="text-2xl text-white font-bold">Find Your Perfect Stay at HotelHaven.</h5> */}
+        <p className="text-2xl text-white">
+          Search low prices on hotels for your dream vacation...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContextProvider } from "./contexts/AppContext";
+import { SearchContextProvider } from "./contexts/SearchContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AppContextProvider>
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
+      </AppContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
