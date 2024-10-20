f9b2a91c574cf183ce20f23e9f22608562cceae2
duchadev <95076379+hannguu@users.noreply.github.com>
2024-10-20 21:56:40 +0700

    feedback BE

---
+++ backend/src/models/feedback.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+// Tạo schema cho feedback
+const feedbackSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Tham chiếu đến UserSchema
+      required: true,
+    },
+    hotelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel", // Tham chiếu đến HotelSchema
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5, // Giới hạn đánh giá sao từ 1 đến 5
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Tạo model dựa trên schema
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+export default Feedback;
